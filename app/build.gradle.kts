99af953a60b6a3cff3d9b4f2cf8b79d498ba15a2
mostafa.fadali <m.fadaly@52.gmail.com>
2025-01-12 20:48:28 +0200

    add unit test but not work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -77,10 +77,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.01"))
+  //  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.6")
+    debugImplementation("androidx.compose.ui:ui-tooling-android:1.7.6")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.6")
 }
 // Allow references to generated code
 kapt {
---
+++ app/src/test/java/com/example/gymlocator/GymsScreenTest.kt
@@ -0,0 +1,12 @@
+//package com.example.gymlocator
+//
+//import org.junit.Rule
+//import androidx.compose.ui.test.junit4.createComposeRule
+//import androidx.compose.ui.test.junit4.ComposeContentTestRule
+//
+//class GymsScreenTest {
+//
+//    @get : Rule
+//    val testRule: ComposeContentTestRule = createComposeRule()
+//}
+//
